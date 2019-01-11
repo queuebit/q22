b77f4124c6ad9eab25f85e7724b2bfc8256b05de
queuebit <queuebit10648@gmail.com>
2019-01-10 23:55:34 -0500

    Reskin app for q22.

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -31,11 +31,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Indiana
+              building <em>"things"</em>.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Twitter
               </a>
             </p>
           </div>
@@ -47,7 +47,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
