803eb32ba6ab26b15ac2d0f09de1402f78a12f8b
queuebit <queuebit10648@gmail.com>
2019-01-20 22:11:18 -0500

    Add micro.blog verification to template

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,7 +60,9 @@ function SEO({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          />
+          >
+            <link href="https://micro.blog/queuebit" rel="me" />
+          </Helmet>
         )
       }}
     />
